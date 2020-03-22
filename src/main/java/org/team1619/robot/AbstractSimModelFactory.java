d2279ad555a3d26bd405e334693a5c8df58da8e9
jaseychanders <31493605+jaseychanders@users.noreply.github.com>
2020-03-22 12:52:42 -0600

    Renamed model factories (#6)

--- src/main/java/org/team1619/robot/AbstractSimModelFactory.java
+++ src/main/java/org/team1619/robot/AbstractSimModelFactory.java
@@ -1,45 +1,43 @@
 package org.team1619.robot;
 
-import org.team1619.models.inputs.bool.robot.RobotControllerButton;
-import org.team1619.models.inputs.bool.robot.RobotDigitalSensor;
-import org.team1619.models.inputs.bool.robot.RobotJoystickButton;
-import org.team1619.models.inputs.numeric.robot.RobotAnalogSensor;
-import org.team1619.models.inputs.numeric.robot.RobotControllerAxis;
-import org.team1619.models.inputs.numeric.robot.RobotJoystickAxis;
+import org.team1619.models.inputs.bool.sim.SimControllerButton;
+import org.team1619.models.inputs.bool.sim.SimDigitalSensor;
+import org.team1619.models.inputs.numeric.sim.SimAnalogSensor;
+import org.team1619.models.inputs.numeric.sim.SimAxis;
 import org.team1619.models.inputs.vector.Odometry;
-import org.team1619.models.inputs.vector.robot.RobotAcceleration;
-import org.team1619.models.inputs.vector.robot.RobotLimelight;
-import org.team1619.models.inputs.vector.robot.RobotNavx;
-import org.team1619.models.outputs.bool.robot.RobotSolenoidDouble;
-import org.team1619.models.outputs.bool.robot.RobotSolenoidSingle;
+import org.team1619.models.inputs.vector.sim.SimAcceleration;
+import org.team1619.models.inputs.vector.sim.SimLimelight;
+import org.team1619.models.inputs.vector.sim.SimNavx;
+import org.team1619.models.outputs.bool.sim.SimSolenoidDouble;
+import org.team1619.models.outputs.bool.sim.SimSolenoidSingle;
 import org.team1619.models.outputs.numeric.MotorGroup;
-import org.team1619.models.outputs.numeric.robot.RobotRumble;
-import org.team1619.models.outputs.numeric.robot.RobotServo;
-import org.team1619.models.outputs.numeric.robot.RobotTalon;
-import org.team1619.models.outputs.numeric.robot.RobotVictor;
+import org.team1619.models.outputs.numeric.sim.SimRumble;
+import org.team1619.models.outputs.numeric.sim.SimServo;
+import org.team1619.models.outputs.numeric.sim.SimTalon;
+import org.team1619.models.outputs.numeric.sim.SimVictor;
 import org.uacr.models.inputs.bool.InputBoolean;
 import org.uacr.models.inputs.numeric.InputNumeric;
 import org.uacr.models.inputs.vector.InputVector;
 import org.uacr.models.outputs.bool.OutputBoolean;
 import org.uacr.models.outputs.numeric.OutputNumeric;
 import org.uacr.robot.AbstractModelFactory;
-import org.uacr.shared.abstractions.InputValues;
-import org.uacr.shared.abstractions.ObjectsDirectory;
-import org.uacr.shared.abstractions.OutputValues;
-import org.uacr.shared.abstractions.RobotConfiguration;
+import org.uacr.shared.abstractions.*;
 import org.uacr.utilities.Config;
 import org.uacr.utilities.YamlConfigParser;
 import org.uacr.utilities.injection.Inject;
 import org.uacr.utilities.logging.LogManager;
 import org.uacr.utilities.logging.Logger;
 
-public class RobotModelFactory extends AbstractModelFactory {
+public class AbstractSimModelFactory extends AbstractModelFactory {
 
-    private static final Logger sLogger = LogManager.getLogger(RobotModelFactory.class);
+    private static final Logger sLogger = LogManager.getLogger(AbstractSimModelFactory.class);
+
+    private final EventBus fEventBus;
 
     @Inject
-    public RobotModelFactory(InputValues inputValues, OutputValues outputValues, RobotConfiguration robotConfiguration, ObjectsDirectory objectsDirectory) {
+    public AbstractSimModelFactory(EventBus eventBus, InputValues inputValues, OutputValues outputValues, RobotConfiguration robotConfiguration, ObjectsDirectory objectsDirectory) {
         super(inputValues, outputValues, robotConfiguration, objectsDirectory);
+        fEventBus = eventBus;
     }
 
     @Override
@@ -48,15 +46,15 @@ public class RobotModelFactory extends AbstractModelFactory {
 
         switch (config.getType()) {
             case "talon":
-                return new RobotTalon(name, config, fSharedObjectDirectory, fSharedInputValues);
+                return new SimTalon(name, config, fEventBus, fSharedObjectDirectory, fSharedInputValues);
             case "victor":
-                return new RobotVictor(name, config, fSharedObjectDirectory);
+                return new SimVictor(name, config, fSharedObjectDirectory);
             case "motor_group":
                 return new MotorGroup(name, config, parser, this);
             case "servo":
-                return new RobotServo(name, config);
+                return new SimServo(name, config);
             case "rumble":
-                return new RobotRumble(name, config);
+                return new SimRumble(name, config);
             default:
                 return super.createOutputNumeric(name, config, parser);
         }
@@ -65,12 +63,11 @@ public class RobotModelFactory extends AbstractModelFactory {
     @Override
     public OutputBoolean createOutputBoolean(Object name, Config config, YamlConfigParser parser) {
         sLogger.trace("Creating output boolean '{}' of type '{}' with config '{}'", name, config.getType(), config.getData());
-
         switch (config.getType()) {
             case "solenoid_single":
-                return new RobotSolenoidSingle(name, config);
+                return new SimSolenoidSingle(name, config);
             case "solenoid_double":
-                return new RobotSolenoidDouble(name, config);
+                return new SimSolenoidDouble(name, config);
             default:
                 return super.createOutputBoolean(name, config, parser);
         }
@@ -82,11 +79,11 @@ public class RobotModelFactory extends AbstractModelFactory {
 
         switch (config.getType()) {
             case "joystick_button":
-                return new RobotJoystickButton(name, config);
+                return new SimControllerButton(fEventBus, name, config);
             case "controller_button":
-                return new RobotControllerButton(name, config);
+                return new SimControllerButton(fEventBus, name, config);
             case "digital_input":
-                return new RobotDigitalSensor(name, config);
+                return new SimDigitalSensor(fEventBus, name, config);
             default:
                 return super.createInputBoolean(name, config);
         }
@@ -98,11 +95,11 @@ public class RobotModelFactory extends AbstractModelFactory {
 
         switch (config.getType()) {
             case "joystick_axis":
-                return new RobotJoystickAxis(name, config);
+                return new SimAxis(fEventBus, name, config);
             case "controller_axis":
-                return new RobotControllerAxis(name, config);
+                return new SimAxis(fEventBus, name, config);
             case "analog_sensor":
-                return new RobotAnalogSensor(name, config);
+                return new SimAnalogSensor(fEventBus, name, config);
             default:
                 return super.createInputNumeric(name, config);
         }
@@ -114,13 +111,13 @@ public class RobotModelFactory extends AbstractModelFactory {
 
         switch (config.getType()) {
             case "accelerometer_input":
-                return new RobotAcceleration(name, config, fSharedInputValues);
+                return new SimAcceleration(fEventBus, name, config, fSharedInputValues);
             case "odometry_input":
                 return new Odometry(name, config, fSharedInputValues);
             case "limelight":
-                return new RobotLimelight(name, config);
+                return new SimLimelight(fEventBus, name, config);
             case "navx":
-                return new RobotNavx(name, config);
+                return new SimNavx(fEventBus, name, config);
             default:
                 return super.createInputVector(name, config);
         }
