b77ba8006afb53b1c36a5650f572d32a2b57ebe8
Matthew <uni.oates@gmail.com>
2021-01-14 20:30:03 -0700

    Finished adding RobotCode

---
+++ src/main/java/org/team1619/robot/FrcSimRobot.java
@@ -0,0 +1,19 @@
+package org.team1619.robot;
+
+import org.team1619.services.logging.LoggingService;
+import org.team1619.shared.concretions.sim.SimDashboard;
+import org.uacr.robot.RobotCore;
+import org.uacr.services.webdashboard.WebDashboardService;
+import org.uacr.utilities.services.Service;
+
+import java.util.List;
+
+public abstract class FrcSimRobot extends RobotCore {
+
+    @Override
+    protected List<Service> createInfoServices() {
+            return List.of(
+            new LoggingService(fInputValues, fOutputValues, fRobotConfiguration, new SimDashboard()),
+            new WebDashboardService(fEventBus, fFms, fInputValues, fOutputValues, fRobotConfiguration));
+    }
+}
