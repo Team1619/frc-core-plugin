d4f4997208a4742f049cc04bf1feb1e43579dd3b
Harrison Kaufman <harrisonkaufman70@gmail.com>
2021-01-17 01:09:27 -0700

    Adds absolute position ***NOT WORKING***

--- src/main/java/org/team1619/models/inputs/vector/robot/RobotCanCoder.java
+++ src/main/java/org/team1619/models/inputs/vector/robot/RobotCanCoder.java
@@ -8,12 +8,14 @@ import org.team1619.models.inputs.vector.Encoder;
 import org.uacr.utilities.Config;
 import org.uacr.shared.abstractions.HardwareFactory;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class RobotCanCoder extends Encoder {
 
     private final CANCoder canCoder;
-    private CANCoderConfiguration canCoderConfiguration = new CANCoderConfiguration();
+    private final CANCoderConfiguration canCoderConfiguration = new CANCoderConfiguration();
+    private final Map<String, Double> canCoderValues = new HashMap<>();
 
     public RobotCanCoder(Object name, Config config, HardwareFactory hardwareFactory) {
         super(name, config);
@@ -25,16 +27,23 @@ public class RobotCanCoder extends Encoder {
         canCoderConfiguration.unitString = "degrees";
         canCoderConfiguration.initializationStrategy = (bootToAbsolutePosition) ? SensorInitializationStrategy.BootToAbsolutePosition : SensorInitializationStrategy.BootToZero;
         canCoderConfiguration.absoluteSensorRange = (sensorRange360) ? AbsoluteSensorRange.Unsigned_0_to_360 : AbsoluteSensorRange.Signed_PlusMinus180;
-        canCoderConfiguration.sensorDirection = (inverted) ? true : false;
+        canCoderConfiguration.sensorDirection = inverted;
         canCoderConfiguration.magnetOffsetDegrees = magnetOffset;
         canCoder.configAllSettings(canCoderConfiguration);
     }
 
     @Override
     protected Map<String, Double> readHardware() {
-        double position = canCoder.getAbsolutePosition() * positionScalar;
-        double velocity = canCoder.getVelocity() * velocityScalar;
-        return Map.of("position", position, "velocity", velocity);
+        if(readPosition) {
+            canCoderValues.put("position", canCoder.getPosition() * positionScalar);
+        }
+        if (readAbsolutePosition) {
+            canCoderValues.put("absolute_position", canCoder.getAbsolutePosition() * positionScalar);
+        }
+        if (readVelocity){
+            canCoderValues.put("velocity", canCoder.getVelocity() * velocityScalar);
+        }
+        return canCoderValues;
     }
 
     @Override
--- src/main/java/org/team1619/models/inputs/vector/sim/SimCanCoder.java
+++ src/main/java/org/team1619/models/inputs/vector/sim/SimCanCoder.java
@@ -3,9 +3,12 @@ package org.team1619.models.inputs.vector.sim;
 import org.team1619.models.inputs.vector.Encoder;
 import org.uacr.utilities.Config;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class SimCanCoder extends Encoder {
+
+    private final Map<String, Double> canCoderValues = new HashMap<>();
 
     public SimCanCoder(Object name, Config config) {
         super(name, config);
@@ -19,7 +22,16 @@ public class SimCanCoder extends Encoder {
 
     @Override
     protected Map<String, Double> readHardware() {
-            return Map.of("position", 0.0, "velocity", 0.0);
+        if(readPosition) {
+            canCoderValues.put("position", 0.0);
+        }
+        if (readAbsolutePosition) {
+            canCoderValues.put("absolute_position", 0.0);
+        }
+        if (readVelocity){
+            canCoderValues.put("velocity", 0.0);
+        }
+        return canCoderValues;
     }
 
     @Override
