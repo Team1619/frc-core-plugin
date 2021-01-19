9990a527e74a86ebac9dd551434287ee98e80ad6
Matthew <uni.oates@gmail.com>
2021-01-18 17:23:27 -0700

    Updated versions

--- src/main/java/org/team1619/models/outputs/numeric/Talon.java
+++ src/main/java/org/team1619/models/outputs/numeric/Talon.java
@@ -3,80 +3,90 @@ package org.team1619.models.outputs.numeric;
 import org.uacr.shared.abstractions.InputValues;
 import org.uacr.utilities.Config;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Talon is a motor object, which is extended to control talons
  */
 
 public abstract class Talon extends CTREMotor {
 
-    protected final InputValues fSharedInputValues;
-    protected final String fFeedbackDevice;
-    protected final String fPositionInputName;
-    protected final String fVelocityInputName;
-    protected final String fCurrentInputName;
-    protected final String fTemperatureInputName;
-    protected final boolean fHasEncoder;
-    protected final boolean fSensorInverted;
-    protected final boolean fReadPosition;
-    protected final boolean fReadVelocity;
-    protected final boolean fReadCurrent;
-    protected final boolean fReadTemperature;
-    protected final boolean fCurrentLimitEnabled;
-    protected final int fContinuousCurrentLimitAmps;
-    protected final int fPeakCurrentLimitAmps;
-    protected final int fPeakCurrentDurationMilliseconds;
-    protected final double fPercentScalar;
-    protected final double fPositionScalar;
-    protected final double fVelocityScalar;
+    protected final InputValues sharedInputValues;
+    protected final String feedbackDevice;
+    protected final String positionInputName;
+    protected final String velocityInputName;
+    protected final String currentInputName;
+    protected final String temperatureInputName;
+    protected final boolean hasEncoder;
+    protected final boolean sensorInverted;
+    protected final boolean readPosition;
+    protected final boolean readVelocity;
+    protected final boolean readCurrent;
+    protected final boolean readTemperature;
+    protected final boolean currentLimitEnabled;
+    protected final int continuousCurrentLimitAmps;
+    protected final int peakCurrentLimitAmps;
+    protected final int peakCurrentDurationMilliseconds;
+    protected final double percentScalar;
+    protected final double positionScalar;
+    protected final double velocityScalar;
+    protected final Map<String, Map<String, Double>> profiles;
+
+    protected String currentProfileName;
 
     public Talon(Object name, Config config, InputValues inputValues) {
         super(name, config);
 
-        fSharedInputValues = inputValues;
+        sharedInputValues = inputValues;
 
-        fFeedbackDevice = config.getString("feedback_device", "");
-        fHasEncoder = !fFeedbackDevice.isEmpty();
-        fSensorInverted = config.getBoolean("sensor_inverted", false);
-        fReadPosition = config.getBoolean("read_position", false);
-        fReadVelocity = config.getBoolean("read_velocity", false);
-        fReadCurrent = config.getBoolean("read_current", false);
-        fReadTemperature = config.getBoolean("read_temperature", false);
-        fCurrentLimitEnabled = config.getBoolean("current_limit_enabled", false);
-        fContinuousCurrentLimitAmps = config.getInt("continuous_current_limit_amps", 0);
-        fPeakCurrentLimitAmps = config.getInt("peak_current_limit_amps", 0);
-        fPercentScalar = config.getDouble("percent_scalar", 1.0);
-        fPositionScalar = config.getDouble("position_scalar", 1.0);
-        fVelocityScalar = config.getDouble("velocity_scalar", 1.0);
-        fPeakCurrentDurationMilliseconds = config.getInt("peak_current_duration_milliseconds", 0);
-        fPositionInputName = config.getString("position_input_name", name.toString().replaceFirst("opn_", "ipn_") + "_position");
-        fVelocityInputName = config.getString("velocity_input_name", name.toString().replaceFirst("opn_", "ipn_") + "_velocity");
-        fCurrentInputName = config.getString("current_input_name", name.toString().replaceFirst("opn_", "ipn_") + "_current");
-        fTemperatureInputName = config.getString("temperature_input_name", name.toString().replaceFirst("opn_", "ipn_") + "_temperature");
+        feedbackDevice = config.getString("feedback_device", "");
+        hasEncoder = !feedbackDevice.isEmpty();
+        sensorInverted = config.getBoolean("sensor_inverted", false);
+        readPosition = config.getBoolean("read_position", false);
+        readVelocity = config.getBoolean("read_velocity", false);
+        readCurrent = config.getBoolean("read_current", false);
+        readTemperature = config.getBoolean("read_temperature", false);
+        currentLimitEnabled = config.getBoolean("current_limit_enabled", false);
+        continuousCurrentLimitAmps = config.getInt("continuous_current_limit_amps", 0);
+        peakCurrentLimitAmps = config.getInt("peak_current_limit_amps", 0);
+        percentScalar = config.getDouble("percent_scalar", 1.0);
+        positionScalar = config.getDouble("position_scalar", 1.0);
+        velocityScalar = config.getDouble("velocity_scalar", 1.0);
+        peakCurrentDurationMilliseconds = config.getInt("peak_current_duration_milliseconds", 0);
+        positionInputName = config.getString("position_input_name", name.toString().replaceFirst("opn_", "ipn_") + "_position");
+        velocityInputName = config.getString("velocity_input_name", name.toString().replaceFirst("opn_", "ipn_") + "_velocity");
+        currentInputName = config.getString("current_input_name", name.toString().replaceFirst("opn_", "ipn_") + "_current");
+        temperatureInputName = config.getString("temperature_input_name", name.toString().replaceFirst("opn_", "ipn_") + "_temperature");
 
+        if (!(config.get("profiles", new HashMap<>()) instanceof Map)) throw new RuntimeException();
+        profiles = (Map<String, Map<String, Double>>) config.get("profiles", new HashMap<>());
+
+        currentProfileName = "none";
     }
 
     // Read the encoder's position
     protected void readEncoderPosition() {
-        if (!fHasEncoder) {
+        if (!hasEncoder) {
             return;
         }
-        fSharedInputValues.setNumeric(fPositionInputName, getSensorPosition());
+        sharedInputValues.setNumeric(positionInputName, getSensorPosition());
     }
 
     // Read the encoder's velocity
     protected void readEncoderVelocity() {
-        if (!fHasEncoder) {
+        if (!hasEncoder) {
             return;
         }
-        fSharedInputValues.setNumeric(fVelocityInputName, getSensorVelocity());
+        sharedInputValues.setNumeric(velocityInputName, getSensorVelocity());
     }
 
     protected void readMotorCurrent() {
-        fSharedInputValues.setNumeric(fCurrentInputName, getMotorCurrent());
+        sharedInputValues.setNumeric(currentInputName, getMotorCurrent());
     }
 
     protected void readMotorTemperature() {
-        fSharedInputValues.setNumeric(fTemperatureInputName, getMotorTemperature());
+        sharedInputValues.setNumeric(temperatureInputName, getMotorTemperature());
     }
 
     public abstract double getSensorPosition();
