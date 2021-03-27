c430a22d786371248f5603114380aa413c724dab
Matthew <uni.oates@gmail.com>
2021-03-27 16:40:43 -0600

    Removed logging

--- src/main/java/org/team1619/models/inputs/vector/OdometryFuser.java
+++ src/main/java/org/team1619/models/inputs/vector/OdometryFuser.java
@@ -63,13 +63,10 @@ public class OdometryFuser extends BaseOdometry {
             if(!absoluteOdometryPosition.equals(lastAbsoluteOdometryPosition)) {
                 movementBuffer.values().stream().reduce((t, v) -> new Vector(t.add(v))).ifPresent(t -> {
                     relativeOdometryOffset = new Vector(absoluteOdometryPosition.add(t).subtract(relativeOdometryPosition));
-//                    LOGGER.info("RESET!!! {}, {}, {}, {}, {}", relativeOdometryPosition, relativeOdometryDelta, absoluteOdometryPosition, t, relativeOdometryOffset);
                 });
                 lastAbsoluteOdometryPosition = absoluteOdometryPosition;
             }
         }
-
-//        LOGGER.info("RELATIVE: {}   OUTPUT: {}", relativeOdometryPosition, relativeOdometryPosition.add(relativeOdometryOffset));
 
         return new Pose2d(relativeOdometryPosition.add(relativeOdometryOffset), relativeOdometryValues.get("heading"));
     }
